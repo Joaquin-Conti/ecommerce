acfe3be1fe4727fe27e05973e5b82eb0b0cb4469
Joaquin Conti <joacoconti@hotmail.com>
2020-07-30 00:37:17 -0300

    adds burger button

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import classes from './Layout.css'
 import Navbar from '../Navbar/Navbar'
+import Content from '../../containers/Content/Content'
 
 export default function Layout() {
     return (
-        <Navbar />
+        <React.Fragment>
+            <Navbar />
+            <Content />
+        </React.Fragment>
     )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './Navbar.css'
 import Logo from '../Logo/Logo.js'
+import SearchBar from '../../containers/SearchBar/SearchBar'
+import Sidebar from '../../containers/Sidebar/Sidebar';
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <nav className="navbar">
             <Logo />
-            <button className='burger-btn'>
-                <i class="fas fa-bars fa-3x"></i>
-            </button>
+            <div onClick={() => setIsOpen(!isOpen)} className={"menu-btn" + (isOpen ? ' open' : '')}>
+                <div className="menu-btn__burger"></div>
+            </div>
+            
             <div className='desktop'>
-                <div>Search</div>
+                <SearchBar />
                 <ul className="nav-items">
                     <a href="#">
                         <li>PRODUCTOS</li>
