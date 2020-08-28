e6961d41cc5ed259c5e3711abf2b1da606e549d4
Joaquin Conti <joacoconti@hotmail.com>
2020-08-28 18:18:34 -0300

    Updates

---
+++ precache-manifest.cc997ea907ad2176141066842ed2dced.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e18e0e3116cfd33574f045d4e2fbe789",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "1956f52f567713113297",
+    "url": "/ecommerce/static/css/main.15a64258.chunk.css"
+  },
+  {
+    "revision": "dc5799c3f336c12ca358",
+    "url": "/ecommerce/static/js/2.61628d53.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ecommerce/static/js/2.61628d53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1956f52f567713113297",
+    "url": "/ecommerce/static/js/main.f12fc40a.chunk.js"
+  },
+  {
+    "revision": "9559208f4a45960884ca",
+    "url": "/ecommerce/static/js/runtime-main.e7519e35.js"
+  },
+  {
+    "revision": "fa3c0e35a62b456c171b61ceb81863f6",
+    "url": "/ecommerce/static/media/blue-hoodie-kotk.fa3c0e35.jpg"
+  },
+  {
+    "revision": "bf130acbe37e772f031aacf9a520a482",
+    "url": "/ecommerce/static/media/got-hoodie.bf130acb.jpg"
+  },
+  {
+    "revision": "a17f6b83f8440722a2262d140d263ecb",
+    "url": "/ecommerce/static/media/hoodie-pink-floyd.a17f6b83.jpg"
+  },
+  {
+    "revision": "8252c00321367d59c0adf2654b8b95ec",
+    "url": "/ecommerce/static/media/jean-negro.8252c003.jpeg"
+  },
+  {
+    "revision": "29b016b5263fa8a34605419d4de51b9f",
+    "url": "/ecommerce/static/media/keo-logo.29b016b5.jpeg"
+  },
+  {
+    "revision": "282bb94ca77a4671bec7e852eb62140e",
+    "url": "/ecommerce/static/media/remera-brba.282bb94c.jpg"
+  },
+  {
+    "revision": "fb78a3d6ab8cf662cfe4d83b820c932f",
+    "url": "/ecommerce/static/media/remera-got1.fb78a3d6.jpg"
+  }
+]);
